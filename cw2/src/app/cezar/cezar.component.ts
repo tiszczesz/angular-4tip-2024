8af298803ccd284905a9c7cca2db59efdf88c58f
Your Name <you@example.com>
2024-11-07 10:25:17 +0100

    jj

--- cw2/src/app/cezar/cezar.component.ts
+++ cw2/src/app/cezar/cezar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-cezar',
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './cezar.component.css'
 })
 export class CezarComponent {
-
+  handleInputKey($event: Event) {
+    this.key = signal(parseInt((<HTMLInputElement>$event.target).value));
+  }
+  handleInput($event: Event) {
+    this.text = signal((<HTMLInputElement>$event.target).value);
+  }
+  text = signal('');
+  key = signal(2);
+  cezarcompute(text: string, key: number) {
+    return text.split('').map((c) => {
+      if (c >= 'a' && c <= 'z') {
+        return String.fromCharCode((c.charCodeAt(0) - 97 + key) % 26 + 97);
+      }
+      if (c >= 'A' && c <= 'Z') {
+        return String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65);
+      }
+      return c;
+    }).join('');
+  }
+  resultText = computed(() => this.cezarcompute(this.text(), this.key()));
 }
