e5f0d7db08ffe65bdd78aee4bb0e1a5ae71cd25c
Your Name <you@example.com>
2024-11-14 09:56:25 +0100

    kk

--- cw3/src/app/courses-list/courses-list.component.ts
+++ cw3/src/app/courses-list/courses-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { Course,courses } from '../../models/courses';
+import { Course, courses } from '../../models/courses';
 
 
 @Component({
@@ -10,5 +10,23 @@ import { Course,courses } from '../../models/courses';
   styleUrl: './courses-list.component.css'
 })
 export class CoursesListComponent {
+  removeCourse(id: number) {
+    this.myCourses.update(courses => courses.filter((value, i) => i !== id));
+  }
+  cities = ['Warszawa', 'Kraków', 'Wrocław', 'Gdańsk', 'Katowice', 'Poznań'];
+  addCourse(courseName: HTMLInputElement, couseDate: HTMLInputElement, courseCity: HTMLSelectElement) {
+    if(courseName.value === '' || couseDate.value === '' || courseCity.value === '') {
+      return;
+    }
+    this.myCourses.update(
+      courses => [...courses, {
+        name: courseName.value,
+        date: new Date(couseDate.value),
+        city: courseCity.value
+      }]);
+    courseName.value = '';
+    couseDate.value = '';
+    
+  }
   myCourses = signal<Course[]>(courses)
 }
