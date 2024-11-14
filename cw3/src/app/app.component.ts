3521e90257d7d9caccc5001ec1cba4eb76e758df
Your Name <you@example.com>
2024-11-14 08:36:19 +0100

    kkk

--- cw3/src/app/app.component.ts
+++ cw3/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SimpleListComponent } from './simple-list/simple-list.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SimpleListComponent],
+  imports: [RouterOutlet,SimpleListComponent,CoursesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- cw3/src/app/courses-list/courses-list.component.ts
+++ cw3/src/app/courses-list/courses-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Course,courses } from '../../models/courses';
+
 
 @Component({
   selector: 'app-courses-list',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './courses-list.component.css'
 })
 export class CoursesListComponent {
-
+  myCourses = signal<Course[]>(courses)
 }
