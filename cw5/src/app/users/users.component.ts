ca59c3f36379554f2ec5dd8ab3c698fd2fb28aa0
Your Name <you@example.com>
2024-12-06 08:56:06 +0100

    tt

--- cw5/src/app/users/users.component.ts
+++ cw5/src/app/users/users.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UserComponent } from './user/user.component';
+import { User, users } from '../models/data_users';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [UserComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-
+  usersList = signal<User[]>(users);
 }
