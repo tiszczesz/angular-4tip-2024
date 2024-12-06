8ea66124ec22d43c5b2157d5a8e40fe89f2b611b
Your Name <you@example.com>
2024-12-06 08:19:13 +0100

    kk

--- cw5/src/app/models/data_users.ts
+++ cw5/src/app/models/data_users.ts
@@ -1,7 +1,45 @@
 export type User = {
-    id: number;
+    id: string;
     name: string;
-    email: string;
-    phone: string;
-    image: string;
+    email: string;   
+    avatar: string;
 }
+export const users: User[] = [
+    {
+        id: 'u1',
+        name: 'Jasmine Washington',
+        email:'user1@exapmple.com',
+        avatar: 'user-1.jpg',
+      },
+      {
+        id: 'u2',
+        name: 'Emily Thompson',
+        email:'user2@exapmple.com',
+        avatar: 'user-2.jpg',
+      },
+      {
+        id: 'u3',
+        name: 'Marcus Johnson',
+        email:'user3@exapmple.com',
+        avatar: 'user-3.jpg',
+      },
+      {
+        id: 'u4',
+        name: 'David Miller',
+        email:'user4@exapmple.com',
+        avatar: 'user-4.jpg',
+      },
+      {
+        id: 'u5',
+        name: 'Priya Patel',
+        email:'user5@exapmple.com',
+        avatar: 'user-5.jpg',
+      },
+      {
+        id: 'u6',
+        name: 'Arjun Singh',
+        email:'user6@exapmple.com',
+        avatar: 'user-6.jpg',
+      },
+
+];
