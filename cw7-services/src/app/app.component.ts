532a56d2cd9aee9c65c73b46596794767b29edd1
Your Name <you@example.com>
2025-02-21 12:30:23 +0100

    kk

--- cw7-services/src/app/app.component.ts
+++ cw7-services/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Post } from '../Models/postsType';
+import { PostsServiceService } from './posts-service.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cw7-services';
+  posts = signal<Post[]>([]);
+  postsService = inject(PostsServiceService);
+
+  async loadPostsToApp() {
+    const posts = await this.postsService.loadPosts();
+    this.posts.set(posts);
+  }
+  /**
+   *
+   */
+  constructor() {
+    this.loadPostsToApp().then((p)=>console.log(p));
+      
+  }
 }
--- cw7-services/src/app/posts-service.service.ts
+++ cw7-services/src/app/posts-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../Models/postsType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsServiceService {
-
-  
+  url = 'https://jsonplaceholder.typicode.com/posts';
+  async loadPosts():Promise<Post[]> {
+    const response = await fetch(this.url);
+    const posts:Post[] = await response.json();
+    return posts;
+  }
 }
